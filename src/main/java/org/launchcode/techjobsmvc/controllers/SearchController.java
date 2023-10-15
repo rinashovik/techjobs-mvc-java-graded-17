8bc11d7e63cba70e13423b87d754b03d66454665
rinashovik <rinashovik@gmail.com>
2023-10-15 12:39:31 -0500

    Final commit after little change

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -24,6 +24,7 @@ public class SearchController {
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+
        return "search";
         //return "redirect:";
     }
@@ -43,6 +44,7 @@ public class SearchController {
             model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
         }
         model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
             return "search";
        // return "redirect:";
 
